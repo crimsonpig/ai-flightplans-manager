9f91a41636c29be467a0440be53057e4864b09ca
crimsonpig <keith.stabbert@yahoo.com>
2014-09-08 22:58:42 -0700

    Removing unused Json libraries.

--- airport-web/src/main/scala/com/keith/fs/airport/domain/AirportJsonProtocol.scala
+++
@@ -1,7 +0,0 @@
-package com.keith.fs.airport.domain
-
-import spray.json.DefaultJsonProtocol
-
-object AirportJsonProtocol extends DefaultJsonProtocol {
-	implicit val airportFormat = jsonFormat4(Airport)
-}
